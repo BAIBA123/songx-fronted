40b57b93b3155406d48ed787fe6dd0fce51055e2
songxin <20013887@suning.com>
2020-09-03 19:16:42 +0800

    路由懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import Index from './pages/index/Index'
-import Read from './pages/read/Index'
-import Store from './pages/store/Index'
-import PostList from './pages/post/Index'
-import PostDetail from './pages/post/Detail'
-import NotFound from './pages/others/NotFound'
+
 import Layout from './components/layout/Index'
 import ScrollToTop from './components/ScrollTop'
+// import Index from './pages/index/Index'
+// import Read from './pages/read/Index'
+// import Store from './pages/store/Index'
+// import PostList from './pages/post/Index'
+// import PostDetail from './pages/post/Detail'
+// import NotFound from './pages/others/NotFound'
+import loadable from './utils/loadable/Index'
+const Index = loadable(() => import('./pages/index/Index'))
+const Read = loadable(() => import('./pages/post/Index'))
+const Store = loadable(() => import('./pages/store/Index'))
+const PostList = loadable(() => import('./pages/post/Index'))
+const PostDetail = loadable(() => import('./pages/post/Detail'))
+const NotFound = loadable(() => import('./pages/others/NotFound'))
 
 function App () {
   return (
